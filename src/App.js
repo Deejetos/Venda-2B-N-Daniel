005921db07bf86338df5eef8fa82738154ca2b6f
andreluissantosdecamargo <gomessiqueiranicolas@gmail.com>
2024-09-25 09:36:04 -0300

    Daniel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import AddProducts from "./pages/AddProduct";
-import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
+import Header from './components/Header';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import AddProduct from './pages/AddProduct';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -12,9 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtos" element={<Products />} />
-        <Route path="/cadastrar-produto" element={<AddProducts />} />
-        <Route path="/Carinho" element={<Cart />} />
+        <Route path="/cadastrar-produto" element={<AddProduct/>} />
+        <Route path="/carrinho" element={<Cart />} />
       </Routes>
     </Router>
-  )
+  );
 }
+
+export default App;
